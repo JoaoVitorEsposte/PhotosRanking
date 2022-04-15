c3a90d7f7498bde70d750e3fb91b738ff1dd8fff
João Vitor Esposte Campos <102496892+JoaoVitorEsposte@users.noreply.github.com>
2022-04-14 22:58:55 -0300

    JS aplicado com sucesso ao menu de seleção de categorias de fotos
    Agora é o JavaScript o responsável por determinar quais fotos irão aparecer na página principal, e o projeto não depende mais de páginas específicas para cada categoria, isso deixou ele muito mais eficiente e fácil de expandir. Aprendi proriedades e funções novas do JS, como o appendChild, removeChild e childElementCount, além de ter aprimorado a minha lógica de programação aplicada ao Front-End.

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
 //Recebe todas as fotos da página principal;
 let listaFotos = document.querySelectorAll(".fotos");
 
+//Recebe a <ul> da lista de fotos;
 let listaPrincipal = document.querySelector(".lista-fotos");
 
 //Recebe todas as opções do menu;
 let listaOpcoes = document.querySelectorAll(".opcao");
 
-//Função que seleciona quais fotos serão removidas da página;
-function selecionaCategoriaFoto (categoria) {
-	if (categoria === "todas"){
-		for(let i = 0; i < listaFotos.length; i++){
+//Define uma constante que armazena o número total de elementos filhos da listaPrincipal;
+const tamanhoListaPrincipal = listaPrincipal.childElementCount;
 
+//Define variável para auxiliar na seleção de fotos a serem exibidas;
+let categoriaAnterior = "todas";
+
+//Reseta a listaPrincipal com todos os elementos originais para possibilitar a seleção de categoria das fotos;
+function completaListaPrincipal () {
+	for (let i = 0; i < listaFotos.length; i++) {
+		listaPrincipal.appendChild(listaFotos[i]);
+	}
+}
+
+//Função que seleciona quais fotos serão exibidas da página;
+function selecionaCategoriaFoto (categoria) {
+	if (categoriaAnterior !== categoria){
+		if(categoria === "todas"){
+			completaListaPrincipal();
+			categoriaAnterior = categoria;
+		}else if(categoria === "objeto"){
+			completaListaPrincipal();
+			for (let i = 0; i < listaFotos.length; i++) {
+				if(listaFotos[i].classList[1] !== "objeto"){
+					listaPrincipal.removeChild(listaFotos[i]);
+				}
+			}
+			categoriaAnterior = categoria;
+		}else if(categoria === "planta"){
+			completaListaPrincipal();
+			for(let i = 0; i < listaFotos.length; i++){
+				if(listaFotos[i].classList[1] !== "planta"){
+					listaPrincipal.removeChild(listaFotos[i]);
+				}
+			}
+			categoriaAnterior = categoria;
 		}
 	}
 }
